9e973c5deb42aee3ad773a9d17865aa49444dfac
”Egor <filaag3@.mail.ru>
2024-02-18 00:12:29 +0300

    [200~

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+for (int i = 1; i < 10; i++)
+{
+    Console.WriteLine(i);
+    
+}
+
+int u = 9;
+
+while (u > 0)
+{
+    Console.WriteLine(u);
+    u--;
+}
+Console.WriteLine("Enter the number * to ээ pyramid: ");
+
+int p = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < p; i++)
+{
+    Console.WriteLine();
+
+    for (int j = 0; j <= i; j++)
+    {
+        Console.Write("*");
+    }
+}
+
